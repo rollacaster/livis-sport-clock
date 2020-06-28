c788c6e6f5ccf74540883441d65d9096321c070e
Thomas Sojka <thsojka@web.de>
2020-06-28 11:13:44 +0200

    Initial verison

--- App.js
+++ App.js
@@ -1,19 +1,127 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { activateKeepAwake, deactivateKeepAwake } from 'expo-keep-awake'
+import { Text, View, TouchableHighlight } from 'react-native'
+import tailwind from 'tailwind-rn'
+import { Audio } from 'expo-av'
+
+const Button = ({ children, onClick }) => (
+  <TouchableHighlight
+    style={tailwind(
+      'bg-orange-400 w-16 h-16 rounded-full items-center justify-center'
+    )}
+    onPress={onClick}
+  >
+    {children}
+  </TouchableHighlight>
+)
+
+const Button2 = ({ children, onClick }) => (
+  <TouchableHighlight
+    style={tailwind('bg-orange-400 p-2 rounded-lg')}
+    onPress={onClick}
+  >
+    {children}
+  </TouchableHighlight>
+)
 
 export default function App() {
+  const [clock, setClock] = useState(10)
+  const [pauseClock, setPauseClock] = useState(10)
+  const [running, setRunning] = useState(false)
+  const [pause, setPause] = useState(false)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (clock === 0) {
+        deactivateKeepAwake()
+
+        Audio.Sound.createAsync(require('./assets/wistle.wav'), {
+          shouldPlay: true
+        })
+          .then(() =>
+            Audio.Sound.createAsync(require('./assets/wistle.wav'), {
+              shouldPlay: true
+            })
+          )
+          .then(() =>
+            Audio.Sound.createAsync(require('./assets/wistle.wav'), {
+              shouldPlay: true
+            })
+          )
+        setRunning(false)
+        setClock(10)
+      } else if (pauseClock === 0) {
+        setPause(false)
+        setPauseClock(10)
+        setClock(clock - 1)
+        Audio.Sound.createAsync(require('./assets/wistle.wav'), {
+          shouldPlay: true
+        })
+      } else if (pause) {
+        setPauseClock(pauseClock - 1)
+      } else if (running && clock % 60 === 0) {
+        Audio.Sound.createAsync(require('./assets/wistle.wav'), {
+          shouldPlay: true
+        })
+        setPause(true)
+      } else if (running) {
+        setClock(clock - 1)
+      }
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [pause, pauseClock, clock, running])
+  console.log({ running, clock, pause, pauseClock })
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={tailwind('flex-1 items-center justify-center')}>
+      {!running ? (
+        <>
+          <View style={tailwind('flex-row items-center mb-5')}>
+            <Button onClick={() => setClock(clock - 1)}>
+              <Text style={tailwind('text-white text-4xl')}>-</Text>
+            </Button>
+            <Text style={tailwind('text-6xl mx-6 w-20 text-center')}>
+              {clock}
+            </Text>
+            <Button onClick={() => setClock(clock + 1)}>
+              <Text style={tailwind('text-white text-4xl')}>+</Text>
+            </Button>
+          </View>
+          <Button2
+            onClick={() => {
+              activateKeepAwake()
+              setRunning(true)
+              setClock(clock * 60 - 1)
+            }}
+          >
+            <Text style={tailwind('text-white text-4xl')}>start</Text>
+          </Button2>
+        </>
+      ) : (
+        <>
+          {pause ? (
+            <Text style={tailwind('text-6xl text-center text-green-400 mb-5')}>
+              {pauseClock}
+            </Text>
+          ) : (
+            <Text style={tailwind('text-6xl text-center mb-5')}>
+              {Math.floor(clock / 60) +
+                ':' +
+                (clock % 60).toString().padStart(2, '0')}
+            </Text>
+          )}
+          <Button2
+            onClick={() => {
+              setRunning(false)
+              setClock(10)
+              setPause(false)
+              setPauseClock(10)
+            }}
+          >
+            <Text style={tailwind('text-white text-4xl')}>stop</Text>
+          </Button2>
+        </>
+      )}
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
